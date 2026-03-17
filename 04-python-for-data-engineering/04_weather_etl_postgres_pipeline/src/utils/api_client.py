dfbc0a566ba1e3d5d920350dbe40cfd500f8de6b
raimandvi <raimandvi123@gmail.com>
2026-03-17 22:06:27 +0530

    Added api_client python file.

---
+++ 04-python-for-data-engineering/04_weather_etl_postgres_pipeline/src/utils/api_client.py
@@ -0,0 +1,9 @@
+import requests
+
+def fetch_weather(lat, lon):
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
+    
+    response = requests.get(url)
+    data = response.json()
+    
+    return data["current_weather"]
