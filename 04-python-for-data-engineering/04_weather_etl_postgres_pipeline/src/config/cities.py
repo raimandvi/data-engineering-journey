f629c2903625afad74166609f2c1f044fc4ab785
raimandvi <raimandvi123@gmail.com>
2026-03-17 21:52:58 +0530

    Added cities python file.

---
+++ 04-python-for-data-engineering/04_weather_etl_postgres_pipeline/src/config/cities.py
@@ -0,0 +1,6 @@
+CITIES = {
+    "Delhi": {"lat": 28.61, "lon": 77.20},
+    "Mumbai": {"lat": 19.07, "lon": 72.87},
+    "Bangalore": {"lat": 12.97, "lon": 77.59},
+    "Hyderabad": {"lat": 17.38, "lon": 78.48}
+}
